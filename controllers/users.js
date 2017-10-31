a59985c6f6783e5460ef0911da8d8ce1c907dbd3
vladimir fomene <vladimirfomene@gmail.com>
2017-10-31 07:38:32 +0000

    Add methods for deleting and getting all users

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,33 @@
 const User = require('../models/user')
 const Interest = require('../models/interest')
 
-
+/*This function gets a particular user from the database using his/her id.
+It returns a 404 status in case it does not find the user. */
 exports.getUserById = function(req, res){
   User.where('user_id', req.params.id).fetch().then(function(user) {
     return res.json(user);
+  }).catch(function(e){
+    console.log(e.stack);
+    res.json(404, {error: e.message});
+  });
+}
+
+/* This function gets all users from our database users table. */
+exports.getAllUsers = function(req, res){
+  User.fetchAll().then(function(users){
+    return res.json(users);
+  }).catch(function(e){
+    console.log(e.stack)
+    res.json(404, {error: e.message});
+  })
+}
+
+/* This function deletes a user from the database given his user id */
+exports.deleteUser = function(req, res){
+  User.where('user_id', req.params.id).destroy().then(function(user) {
+    return res.json(user);
+  }).catch(function(e){
+    console.log(e.stack)
+    res.json(404, {error: e.message});
   });
 }
